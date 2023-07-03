5f843ea2f518c1e863093df1f1276d60667402d7
VenoM9078 <roushan.venom1@gmail.com>
2023-07-03 17:14:45 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -141,6 +141,31 @@ const Home = () => {
       <div className="relative">
         <div className="relative container m-auto px-6 md:px-12 lg:px-6">
           <div className="mb-12 pt-28 lg:w-9/12 lg:mx-auto">
+            <a
+              href="https://sandapi.com"
+              className="inline-flex mb-6 justify-between items-center  py-1 px-1 pr-4 text-sm text-gray-900 bg-purple-200 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-40 border border-gray-100  hover:bg-purple-200"
+            >
+              <span className="text-xs bg-gradient-to-tr from-purple-500 to-pink-300 rounded-full text-white px-4 py-1.5 mr-3">
+                New
+              </span>{" "}
+              <span className="text-sm font-medium">
+                We are launching Sand API - another solo adventure to make APIs
+                easy for you!
+              </span>
+              <svg
+                aria-hidden="true"
+                className="ml-2 w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </a>
             <h1
               style={{ fontFamily: "Unbounded" }}
               className="text-slate-900 text-center pb-5 text-5xl w-full font-bold sm:text-4xl md:text-5xl"
